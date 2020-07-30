c9ea0d7990369868563175308b66e332e0c8b695
Michael Brière <55116710+Mickybi@users.noreply.github.com>
2020-07-30 14:15:48 -0400

    Update TimeKeeper.py

--- TimeKeeper.py
+++ TimeKeeper.py
@@ -6,10 +6,9 @@ class TimeKeeper(NameKeeper):
     def __init__(self, firstName, lastName, date, time):
         self.date = date
         self.time = time
-        
-    NameKeeper.__init__(self, firstName, lastName)
+        NameKeeper.__init__(self, firstName, lastName) #  Should be indented at the same level as the self variables
     
-    def punchIn():
+    def punchIn(self, x): # There should be a self argument here. Also, x is not declared within the function. We need to pass an x argument for this to work
         # attempting to simulate punching in by using return statement
         return "You have punched in at" + x + "time!"
         
