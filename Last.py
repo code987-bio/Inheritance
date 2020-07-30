f12c414f1b0600aa0242938e2c746dee4cbf8edb
Michael Brière <55116710+Mickybi@users.noreply.github.com>
2020-07-30 14:10:36 -0400

    Update Last.py

--- Last.py
+++ Last.py
@@ -13,7 +13,7 @@ class TimeKeeper3(NameKeeper):
         x = datetime.datetime.now()
         print(x)
     
-    p1 = Employee("John", "Doe")
+    p1 = Employee("John", "Doe")  # There is no class employee. Therefore, this won't compile when you will run your code. Replace with Namekeeper?
     print(p1.firstName, p1.lastName)
     
     
@@ -23,16 +23,15 @@ class TimeKeeper3(NameKeeper):
         employeePunchIn = Timekeeper("John", "Doe", "11/21/2011", "06:30am")
         employeePunchOut = Timekeeper2("John", "Doe", "11/21/2011", "10:30am")
          
-    #method call
-    testMethod(employeePunchIn, employeePunchOut)
+   
     
 # Declaration
     def testMethod(employeePunchIn, employeePunchOut):
         print("John Doe has clocked in at 06:30am on 11/21/2011")
         print("John Doe has clocked out at 10:30am on 11/21/2011")
 
-testMethod():
-print("name keeper attribute 1", NameKeeper.firstName)
+testMethod():  # To test these, you should create an object of a class first. 
+print("name keeper attribute 1", NameKeeper.firstName)  # All these items should be indented
 print("name keeper attribute 2", NameKeeper.lastName)
 print("time keeper attribute 1", TimeKeeper.firstName)
 print("time keeper attribute 2", TimeKeeper.lastName)
@@ -54,5 +53,7 @@ print("time keeper 3 method 1", TimeKeeper3.showPunchOutTime)
 print("time keeper 3 method 2", TimeKeeper3.Main)
 
 # Class invocation
-    Main()
+Main()  # To create a class invocation, you should create an object of that class (e.g. p1 = Timekeeper3("Joe", "Doe", date, time)
     
+#method call
+testMethod(employeePunchIn, employeePunchOut) # Should be moved outside of class. An object should be created first
